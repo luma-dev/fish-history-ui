2a37fb99ed93a5de8238da6b63d51d22622f4ee3
Luma <world@luma.email>
2021-08-22 08:03:27 +0900

    chore: wip

--- projects/web-ui/src/util/__tests__/temporal.spec.ts
+++ projects/web-ui/src/util/__tests__/temporal.spec.ts
@@ -1,111 +1,86 @@
-import {
-  firstDayOfMonth,
-  firstDayOfWeek,
-  lastDayOfMonth,
-  lastDayOfWeek,
-} from "../temporal";
-import { Temporal } from "@js-temporal/polyfill";
+import { Temporal } from '@js-temporal/polyfill';
+import { firstDayOfMonth, firstDayOfWeek, lastDayOfMonth, lastDayOfWeek } from '../temporal';
 
-describe("firstDayOfMonth", () => {
-  it("returns first day of month", () => {
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 2)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 30)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 31)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 2, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 2, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 2, 29)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 2, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 3, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 3, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 12, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 12, 1).toString(),
-      );
-    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 12, 31)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 12, 1).toString(),
-      );
+describe('firstDayOfMonth', () => {
+  it('returns first day of month', () => {
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 2)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 30)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 1, 31)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 2, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 2, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 2, 29)).toString()).toBe(
+      new Temporal.PlainDate(2000, 2, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 3, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 3, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 12, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 12, 1).toString(),
+    );
+    expect(firstDayOfMonth(new Temporal.PlainDate(2000, 12, 31)).toString()).toBe(
+      new Temporal.PlainDate(2000, 12, 1).toString(),
+    );
   });
 });
 
-describe("lastDayOfMonth", () => {
-  it("returns last day of month", () => {
+describe('lastDayOfMonth', () => {
+  it('returns last day of month', () => {
     expect(lastDayOfMonth(new Temporal.PlainDate(2000, 1, 1)).toString()).toBe(
       new Temporal.PlainDate(2000, 1, 31).toString(),
     );
-    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 1, 31)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 31).toString(),
-      );
-    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 2, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 2, 29).toString(),
-      );
-    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 2, 29)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 2, 29).toString(),
-      );
-    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 12, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 12, 31).toString(),
-      );
-    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 12, 31)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 12, 31).toString(),
-      );
+    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 1, 31)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 31).toString(),
+    );
+    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 2, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 2, 29).toString(),
+    );
+    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 2, 29)).toString()).toBe(
+      new Temporal.PlainDate(2000, 2, 29).toString(),
+    );
+    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 12, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 12, 31).toString(),
+    );
+    expect(lastDayOfMonth(new Temporal.PlainDate(2000, 12, 31)).toString()).toBe(
+      new Temporal.PlainDate(2000, 12, 31).toString(),
+    );
   });
 });
 
-describe("firstDayOfWeek", () => {
-  it("returns first day of week", () => {
+describe('firstDayOfWeek', () => {
+  it('returns first day of week', () => {
     // It is Saturday(6) on 2020/1/1
-    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(1999, 12, 26).toString(),
-      );
-    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 1).toString())
-      .toBe(
-        new Temporal.PlainDate(1999, 12, 27).toString(),
-      );
-    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 6).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
+    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1)).toString()).toBe(
+      new Temporal.PlainDate(1999, 12, 26).toString(),
+    );
+    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 1).toString()).toBe(
+      new Temporal.PlainDate(1999, 12, 27).toString(),
+    );
+    expect(firstDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 6).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
   });
 });
 
-describe("lastDayOfWeek", () => {
-  it("returns last day of week", () => {
-    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1)).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 1).toString(),
-      );
-    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 1).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 2).toString(),
-      );
-    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 6).toString())
-      .toBe(
-        new Temporal.PlainDate(2000, 1, 7).toString(),
-      );
+describe('lastDayOfWeek', () => {
+  it('returns last day of week', () => {
+    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1)).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 1).toString(),
+    );
+    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 1).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 2).toString(),
+    );
+    expect(lastDayOfWeek(new Temporal.PlainDate(2000, 1, 1), 6).toString()).toBe(
+      new Temporal.PlainDate(2000, 1, 7).toString(),
+    );
   });
 });
